af1838c0ba91dbf56d14416daacece22cb97ef97
david <davidmatovu88@gmail.com>
2023-03-29 09:08:23 +0300

    styling number input

--- my-game-app/screens/StartGameScreen.js
+++ my-game-app/screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.appContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -23,10 +29,21 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: "#72063c",
     elevation: 4,
-    // add a show to IOS
+    // add a shadow to IOS
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
