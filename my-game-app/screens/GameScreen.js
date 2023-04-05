ecdce8a98582a93174acd9f2a4fc2782c55bc060
david <davidmatovu88@gmail.com>
2023-04-05 09:57:39 +0300

    add button icons

--- my-game-app/screens/GameScreen.js
+++ my-game-app/screens/GameScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import Title from "../components/UI/Title";
 import Card from "../components/UI/Card";
 import NumberContainer from "../components/game/NumberContainer";
@@ -66,12 +67,12 @@ export default function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
+              <Ionicons name="md-remove" size={24} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
-              +
+              <Ionicons name="md-add" size={24} color="white" />
             </PrimaryButton>
           </View>
         </View>
