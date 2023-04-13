0dc8643932fcde68a3efa70dac4f451cc76a9065
david <davidmatovu88@gmail.com>
2023-04-13 09:16:56 +0300

    style the status bar

--- my-game-app/App.js
+++ my-game-app/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { StyleSheet, View, ImageBackground, SafeAreaView } from "react-native";
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
@@ -55,16 +56,21 @@ export default function App() {
 
   return (
     // <LinearGradient color={[Colors.Primary700,Colors.accent500]} style={styles.rootScreen}>
-    <View style={styles.rootScreen}>
-      <ImageBackground
-        source={require("./assets/sprint1.jpg")}
-        resizeMode="cover"
-        style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-      >
-        <SafeAreaView style={styles.safeAreaView}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </View>
+    <>
+      <StatusBar style="light" />
+
+      <View style={styles.rootScreen}>
+        <ImageBackground
+          source={require("./assets/sprint1.jpg")}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.safeAreaView}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </View>
+    </>
+
     //  </LinearGradient>
   );
 }
